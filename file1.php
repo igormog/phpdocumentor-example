aad7c667b8e03be7d69f9a3533918d028bccb9b8
Igor Mogilin <i@igorm.org>
2015-12-27 17:08:42 +0200

    add function sumOfSquares()

--- file1.php
+++ file1.php
@@ -11,3 +11,13 @@
  * @return int
  * @throws Exception If element in array is not an integer
  */
+function sumOfSquares($arr) {
+    $total = 0;
+    foreach ($arr as $val) {
+        if (!is_int($val)) {
+            throw new Exception("Element is not an integer!");
+        }
+        $total += $val * $val;
+    }
+    return $total;
+}
