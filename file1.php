973d8f8e018260f695958e8972f6303334e28edd
Igor Mogilin <i@igorm.org>
2015-12-31 17:11:48 +0200

    add "@package MyProject"

--- file1.php
+++ file1.php
@@ -37,3 +37,9 @@ class SomeClass {
  *
  * The class is empty for the sake of this example.
  *
+ * @package    MyProject
+ * @subpackage Common
+ * @author     Moshe Teutsch <moteutsch@gmail.com>
+ */
+class Example {
+}
