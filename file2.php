3d032085e42079d6aa3da24c97b49373a1f2c261
Igor Mogilin <i@igorm.org>
2016-01-09 17:22:49 +0200

    Local variables

--- file2.php
+++ file2.php
@@ -215,3 +215,11 @@ function setFoo($arg1, $arg2 = 0)
             return false;
         }
     }
+
+/*
+ * Local variables:
+ * tab-width: 4
+ * c-basic-offset: 4
+ * c-hanging-comment-ender-p: nil
+ * End:
+ */
