b0b66dbeab6363b67a25b13871844af0858fe102
Igor Mogilin <i@igorm.org>
2016-01-12 17:25:13 +0200

    return the date of Easter

--- file3.php
+++ file3.php
@@ -5,3 +5,13 @@
 function foo()
 {
 }
+
+
+/**
+* return the date of Easter
+* 
+* Using the formula from "Formulas that are way too complicated for anyone to 
+* ever understand except for me" by Irwin Nerdy, this function calculates the 
+* date of Easter given a date in the Ancient Mayan Calendar, if you can also
+* guess the birthday of the author.
+*/
